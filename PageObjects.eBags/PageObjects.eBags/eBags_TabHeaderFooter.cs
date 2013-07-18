fcde5dbd84b7cb86ac73f1d848af4c44b749d080
Seth <surban@prototest.com>
2013-07-18 13:53:45 -0600

    Initial Push for eBags Automated Tests

---
+++ PageObjects.eBags/PageObjects.eBags/eBags_TabHeaderFooter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Golem.Framework;
+using OpenQA.Selenium;
+
+namespace PageObjects.eBags
+{
+    public class eBags_TabHeaderFooter : BasePageObject
+    {
+
+        public override void WaitForElements()
+        {
+            
+        }
+    }
+}
---
+++ Tests.eBags/eBags_ASplit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Golem.Framework;
+using PageObjects.eBags;
+using MbUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Tests.eBags
+{
+    public class eBags_ASplit : TestBaseClass
+    {
+
+    }
+}
